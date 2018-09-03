97753be702cc6255e42b9ce32e144ddae3651da1
Exo-poulpe <33369156+Exo-poulpe@users.noreply.github.com>
2018-09-03 10:40:10 +0200

    Add to server page the title with JS and appli change to php.ini

--- PhpServer.php
+++ PhpServer.php
@@ -1,7 +1,10 @@
+<script>
+document.title = "Server";
+</script>
 <?php
 
 $textPost = $_REQUEST["textPost"];
 //$filesGet = $_FILES["filePictures"]["name"];
 echo $textPost;
-//var_dump(phpinfo());
+var_dump(phpinfo());
 ?>

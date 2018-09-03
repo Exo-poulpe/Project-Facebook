fe4230c582088ec29c3d2ff1e9a6c5cb916c20c6
Exo-poulpe <33369156+Exo-poulpe@users.noreply.github.com>
2018-09-03 09:57:20 +0200

    Add <br> for design

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 
 <html>
-    <head> 
+    <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="BootStrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="CssPhp.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        
+
         <div class="container">
-            
+
             <div class="row">
                 <div class="col-3">
                     <img src="images/CFPT_logo.png"  alt="CFPT_logo" class="border "/>
@@ -19,20 +19,21 @@
                 <img src="images/Code_image.jpg"  alt="Code_image" class=" img-fluid " />
                 </div>
             </div>
-            
+
         </div>
-        
+
         <form method="post" action="PhpServer.php" enctype="multipart/form-data" id="formulaire">
-            
+
             <textarea rows="5" cols="50" form="formulaire" name="textPost"></textarea>
+            <br>
             <br>
               <input type="file" name="filePictures" accept="image/*"/>
               <br>
               <br>
               <input type="submit" name="btnEnvoyer" value="Choisir un fichier" class="btn btn-outline-secondary" />
         </form>
-        
-        
+
+
     </body>
-    
+
 </html>

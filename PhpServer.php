5633ab099552f6acbe4dfc704ec7f8b9f5d7f2d9
Exo-poulpe <33369156+Exo-poulpe@users.noreply.github.com>
2018-09-05 15:26:17 +0200

    Connect to SQL database with user

--- PhpServer.php
+++ PhpServer.php
@@ -5,6 +5,12 @@ $target_dir = "C:\\Users\\Administrateur\\Documents\\Download_php\\";
 $textPost = $_REQUEST["textPost"];
 //var_dump($_FILES);
 
+DEFINE('DB_HOST','127.0.0.1');
+DEFINE('DB_USER','root');
+DEFINE('DB_NAME','fb_post');
+DEFINE('DB_PASS','');
+
+
 $myFile = $_FILES["filePictures"];
 
 /***********************************/
@@ -20,15 +26,48 @@ for ($i=0; $i < count($myFile["name"]); $i++) {
   else
   {
     //echo $myFile["name"][$i];
-    echo "Chemin temp : " . $myFile["tmp_name"][$i];
-    echo " ";
+    //echo "Chemin temp : " . $myFile["tmp_name"][$i];
+    //echo " ";
     $target_dir .= $myFile["name"][$i];
-    echo "Chemin upload : " . $target_dir;
-    echo " ";
+    //echo "Chemin upload : " . $target_dir;
+    //echo " ";
 
   }
 
+  var_dump(connectToDb());
 
+
+
+
+}
+
+function connectToDb()
+{
+  static $dbb = null;
+
+  if ($dbb === null)
+  {
+    try
+    {
+      $connectionString = 'mysql:host=' . DB_HOST . ';dbname='.DB_NAME . '' ;
+      $dbb = new PDO($connectionString,DB_USER,DB_PASS);
+      $dbb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch (Exception $e)
+    {
+      die('Erreur : ' . $e->getMessage());
+    }
+    return $dbb;
+
+
+  }
+
+}
+
+
+function getMessageFromDb()
+{
+  
 
 
 }
@@ -36,7 +75,7 @@ for ($i=0; $i < count($myFile["name"]); $i++) {
 
 
 //echo $textPost;
-var_dump(phpinfo());
+//var_dump(phpinfo());
 //$filesGet = $_FILES["filePictures"]["name"];
 
 
